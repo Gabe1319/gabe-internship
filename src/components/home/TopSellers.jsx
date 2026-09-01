39bf8c6544de20cac87fbe9cbf0c8e5af9f85d78
Gabriel Litvin <gabealitvin@gmail.com>
2026-09-01 19:21:34 -0400

    why not loading?

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-import { use } from "react";
 import axios from "axios";
 import Skeleton from "../UI/Skeleton";
 
