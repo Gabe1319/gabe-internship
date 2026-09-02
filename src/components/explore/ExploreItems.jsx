e2875fdffc09855771930e9cae4a799781db7a0c
Gabriel Litvin <gabealitvin@gmail.com>
2026-09-01 22:01:44 -0400

    skeleton done

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -43,7 +43,17 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {exploreItems.length && skeletonLoading ? (
+      {skeletonLoading ? (<>
+          {new Array(8).fill(0).map((_, index) => (
+            <div
+              key={index}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <Skeleton width="100%" height="400px" />
+            </div>
+          ))}
+        </>) : (
         exploreItems.slice(0, itemCount).map((item, index) => (
           <div
             key={index}
@@ -102,20 +112,7 @@ const ExploreItems = () => {
             </div>
           </div>
         ))
-      ) : (
-        <>
-          {new Array(8).fill(0).map((_, index) => (
-            <div
-              key={index}
-              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              style={{ display: "block", backgroundSize: "cover" }}
-            >
-              <Skeleton width="100%" height="400px" />
-            </div>
-          ))}
-        </>
       )}
-
       <div className="col-md-12 text-center">
         <Link
           onClick={() => setItemCount(itemCount + 4)}
